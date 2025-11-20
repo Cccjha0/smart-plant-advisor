c6d0b54e93f933ad517a5c3908607734a5832fc1
Cccjha0 <523001610@qq.com>
2025-11-20 17:50:55 +0800

    Add sensor and weight endpoints with DB session dependency

--- backend/database.py
+++ backend/database.py
@@ -9,3 +9,10 @@ engine = create_engine(DB_URL, echo=False, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
